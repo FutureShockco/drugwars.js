670988cffe7f0752aa6dce023e1b806551616d99
adcpm <fabien@bonustrack.co>
2019-03-04 04:00:11 +0700

    Add kbyte

---
+++ index.js
@@ -0,0 +1,9 @@
+const { Client } = require('kbyte');
+const units = require('./units.json');
+const buildings = require('./buildings.json');
+
+module.exports = {
+  Client,
+  units,
+  buildings,
+};
