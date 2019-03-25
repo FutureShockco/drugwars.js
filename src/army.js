3de55105d310c0358173fb8a054a593aed410bbb
adcpm <fabien@bonustrack.co>
2019-03-26 04:41:13 +0700

    Deal damage unit per unit

--- src/army.js
+++ src/army.js
@@ -1,62 +1,71 @@
 import { orderBy } from 'lodash';
-import Troop from './troop';
+import Unit from './unit';
 
 export default class Army {
   constructor(units, name, log) {
-    this.troops = [];
+    this.units = [];
     this.alive = true;
     this.name = name;
     this.log = log;
 
     units.forEach(unit => {
-      this.troops.push(new Troop(unit.key, unit.amount, name, log));
+      for (let i = 0; i < unit.amount; i += 1) {
+        this.units.push(new Unit(unit.key, i + 1, name, log));
+      }
     });
   }
 
   getAttacks() {
     const attacks = [];
 
-    const troopsSorted = orderBy(this.troops, ['priority'], ['asc']);
-    troopsSorted.forEach(troop => {
-      if (troop.undead > 0) {
-        const attack = troop.getAttack();
-        if (attack > 0) {
-          this.log.add(`[${this.name}] ${troop.undead} x ${troop.key} attack +${attack}`);
-          attacks.push(attack);
-        }
+    this.units.forEach(unit => {
+      if (!unit.dead) {
+        this.log.add(`[${this.name}] ${unit.key} (${unit.i}) attack +${unit.spec.attack}`);
+        attacks.push(unit.spec.attack);
       }
     });
+
     return attacks;
   }
 
   takeDamages(damages) {
     const pending = damages;
 
-    const troopsSorted = orderBy(this.troops, ['priority'], ['asc']);
-    troopsSorted.forEach(troop => {
-      if (troop.undead > 0 && pending.length > 0) {
-        troop.takeDamages(pending[0]);
+    const unitsSorted = orderBy(this.units, ['priority'], ['asc']);
+    unitsSorted.forEach(unit => {
+      while (!unit.dead && pending.length > 0) {
+        unit.takeDamages(pending[0]);
         pending.splice(0, 1);
       }
     });
 
-    const troopsAlive = this.troops.filter(troop => troop.undead > 0).length;
-    if (!troopsAlive) {
+    const unitsAlive = this.units.filter(unit => !unit.dead).length;
+    if (!unitsAlive) {
       this.alive = false;
     }
 
-    if (troopsAlive > 0 && pending.length > 0) {
+    if (unitsAlive > 0 && pending.length > 0) {
       this.takeDamages(pending);
     }
   }
 
   getResult() {
-    return this.troops.map(troop => {
-      const unit = {
-        key: troop.key,
-        amount: troop.amount,
-      };
-      if (troop.dead > 0) unit.dead = troop.dead;
+    const unitsObj = {};
+
+    this.units.forEach(unit => {
+      if (!unitsObj[unit.key]) {
+        unitsObj[unit.key] = {
+          amount: 0,
+          dead: 0,
+        };
+      }
+      unitsObj[unit.key].amount += 1;
+      unitsObj[unit.key].dead += unit.dead ? 1 : 0;
+    });
+
+    return Object.keys(unitsObj).map(key => {
+      const unit = { key, amount: unitsObj[key].amount };
+      if (unitsObj[key].dead) unit.dead = unitsObj[key].dead;
       return unit;
     });
   }
