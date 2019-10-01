c6d1d58c3a8dfd38e8466ef91ee9fc11801aa63e
hightouch67 <cetinkaya.ozden@gmail.com>
2019-10-01 18:08:17 +0000

    card test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import { Client } from 'kbyte';
 import constants from './constants';
 import Fight from './fight';
-import Card from './card';
-import CardPack from './cardpack';
 import units from './units.json';
 import buildings from './buildings.json';
 import shop from './shop.json';
@@ -13,12 +11,20 @@ import drugs from './drugs.json';
 import locations from './locations.json';
 import continents from './continents.json';
 import utils from './utils';
+
+import heroes from './heroes.json';
+import suffixes from './suffixes.json';
+import prefixes from './prefixes.json';
+import actives from './actives.json';
+import passives from './passives.json';
+import c_units from './cardunits.json';
+import c_buildings from './cardbuildings.json';
+import c_items from './carditems.json';
+import c_upgrades from './cardupgrades.json';
 module.exports = {
   constants,
   Client,
   Fight,
-  CardPack,
-  Card,
   units,
   buildings,
   shop,
@@ -29,4 +35,5 @@ module.exports = {
   locations,
   continents,
   utils,
+  Cards: { heroes, suffixes, prefixes, passives, actives, unit: c_units, buildings: c_buildings, items: c_items, upgrades: c_upgrades }
 };
