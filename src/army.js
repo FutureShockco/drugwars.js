a7864be0ef95939d8dbcea5ba68b9925dc93cfa5
hightouch67 <cetinkaya.ozden@gmail.com>
2019-12-11 13:19:49 +0100

    update 136

--- src/army.js
+++ src/army.js
@@ -48,70 +48,64 @@ export default class Army {
         // }
       }
     });
-    trainings.forEach(training => {
-      if (training.key === 'routing' || training.lvl < 1) {
-      }
-      else
-        this.trainings.push({ key: training.key, lvl: training.lvl });
-    });
     //ATTRIBUTE TRAINING MODIFICATOR
     this.groups.forEach(unit => {
       // HOBO
       if (unit.key === "hobo") {
-        const kamikaze = this.trainings.find(b => b.key === 'spiritwine' || b.training === 'spiritwine');
+        let kamikaze = this.trainings.find(b => b.key === 'spiritwine');
         if (kamikaze)
           unit.attack = unit.attack + (unit.attack / 100 * kamikaze.lvl);
       }
       else {
-        const protection = this.trainings.find(b => b.key === 'protection' || b.training === 'protection');
+        let protection = this.trainings.find(b => b.key === 'protection');
         if (protection)
           unit.defense = unit.defense + (unit.defense / 5 * protection.lvl);
         //ALL MELEE
         if (unit.spec.type === 'Melee') {
-          const closecombat = this.trainings.find(b => b.key === 'closecombat' || b.training === 'closecombat');
+          let closecombat = this.trainings.find(b => b.key === 'closecombat');
           if (closecombat)
             unit.attack = unit.attack + (unit.attack / 100 * closecombat.lvl);
         }
         else {
-          const firearms = this.trainings.find(b => b.key === 'firearms' || b.training === 'firearms');
+          let firearms = this.trainings.find(b => b.key === 'firearms');
           if (firearms) {
             unit.attack = unit.attack + (unit.attack / 100 * firearms.lvl);
           }
         }
         if (unit.key === "sniper") {
-          const sniping = this.trainings.find(b => b.key === 'sniping' || b.training === 'sniping');
+          let sniping = this.trainings.find(b => b.key === 'sniping');
           if (sniping)
             unit.attack = unit.attack + (unit.attack / 100 * sniping.lvl);
         }
         else if (unit.key === "bazooka") {
-          const bomb = this.trainings.find(b => b.key === 'bomb' || b.training === 'bomb');
+          let bomb = this.trainings.find(b => b.key === 'bomb');
           if (bomb)
             unit.attack = unit.attack + (unit.attack / 100 * bomb.lvl);
         }
         //WEAPON
         else if (unit.key === "rowdy" || unit.key === "sniper" || unit.key === "hitman" || unit.key === "biker") {
-          const firearms = this.trainings.find(b => b.key === 'weapon' || b.training === 'weapon');
+          let firearms = this.trainings.find(b => b.key === 'weapon');
           if (firearms) {
             unit.attack = unit.attack + (unit.attack / 100 * firearms.lvl);
           }
         }
         // FIRE
         if (unit.key === "bazooka" || unit.key === "gunman" || unit.key === "helicopter") {
-          const fire = this.trainings.find(b => b.key === 'fire' || b.training === 'fire');
+          let fire = this.trainings.find(b => b.key === 'fire');
           if (fire)
             unit.attack = unit.attack + (unit.attack / 100 * fire.lvl);
         }
 
         // CHEMICAL
         if (unit.key === "mercenary" || unit.key === "knifer") {
-          const chemical = this.trainings.find(b => b.key === 'chemical' || b.training === 'chemical');
+          let chemical = this.trainings.find(b => b.key === 'chemical');
           if (chemical)
             unit.attack = unit.attack + (unit.attack / 100 * chemical.lvl);
         }
 
         //ELITE
         if (unit.key === "mercenary" || unit.key === "knifer" || unit.key === "big_mama" || unit.key === "ninja") {
-          const psychological = this.trainings.find(b => b.key === 'psychological' || b.training === 'psychological');
+          let psychological = this.trainings.find(b => b.key === 'psychological');
           if (psychological) {
             unit.attack = unit.attack + (unit.attack / 200 * psychological.lvl);
             unit.defense = unit.defense + (unit.defense / 200 * psychological.lvl);
@@ -240,25 +234,25 @@ export default class Army {
             });
             break;
           case 'multi':
-              attack = {}
-              attack.author = name
-              attack.num = num
-              attack.dmg = parseInt(action[0]);
-              serie.push(attack);
-              unitsByHighestPriority.forEach(unit => {
-                if (unit.undead > 0 && serie.length > 0) {
-                  unit.takeGroupDamages(serie[0].dmg * attackpower / 100, skill_type, round, serie[0].author, serie[0].num, undead);
-                  serie.splice(0, 1);
-                }
-              });
-              serie.push(attack);
-              unitsSorted.forEach(unit => {
-                if (unit.undead > 0 && serie.length > 0) {
-                  unit.takeGroupDamages(serie[0].dmg * attackpower / 100, skill_type, round, serie[0].author, serie[0].num, undead);
-                  serie.splice(0, 1);
-                }
-              });
-              break;
+            attack = {}
+            attack.author = name
+            attack.num = num
+            attack.dmg = parseInt(action[0]);
+            serie.push(attack);
+            unitsByHighestPriority.forEach(unit => {
+              if (unit.undead > 0 && serie.length > 0) {
+                unit.takeGroupDamages(serie[0].dmg * attackpower / 100, skill_type, round, serie[0].author, serie[0].num, undead);
+                serie.splice(0, 1);
+              }
+            });
+            serie.push(attack);
+            unitsSorted.forEach(unit => {
+              if (unit.undead > 0 && serie.length > 0) {
+                unit.takeGroupDamages(serie[0].dmg * attackpower / 100, skill_type, round, serie[0].author, serie[0].num, undead);
+                serie.splice(0, 1);
+              }
+            });
+            break;
           default:
             attack = {}
             attack.author = name
@@ -340,7 +334,7 @@ export default class Army {
         supply += dwunits[unit.key].supply * unit.undead
     });
     let power = Math.round(100 - parseFloat(supply / 6).toFixed(0) / 100)
-    const coordination = trainings.find(b => b.key === 'coordination' || b.training === 'coordination');
+    const coordination = trainings.find(b => b.key === 'coordination');
     if (coordination)
       power = power + parseInt(coordination.lvl) / 10
     if (power >= 60) {
@@ -358,7 +352,7 @@ export default class Army {
         supply += dwunits[unit.key].supply * unit.undead
     });
     let power = Math.round(100 - parseFloat(supply / 5).toFixed(0) / 100)
-    const coordination = trainings.find(b => b.key === 'coordination' || b.training === 'coordination');
+    const coordination = trainings.find(b => b.key === 'coordination');
     if (coordination)
       power = power + parseInt(coordination.lvl) / 10
     if (power >= 60) {
